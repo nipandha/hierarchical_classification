6d4871784a054ff1776bd44bbdcfb560b664d8ef
darieMures <vladdobru@gmail.com>
2016-12-17 18:08:24 -0500

    CV code

--- split_data.py
+++ split_data.py
@@ -1,35 +1,121 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 15 12:20:26 2016
+Created on Sun Dec  4 14:49:20 2016
 
 @author: user
 """
-
 from Utils.GeneralUtils import *
-list_of_top=[]
-list_of_data=initialize_list_of_lists(11)
 
-
-for index in range(0, 11):
-    filepath=create_filename_current_directory("Datasets/all_categories_"+str(index))
-    print "Working on: "+filepath
-    f = open(filepath, 'r')
-    lines=f.readlines()
-    f.close()
-    list_of_current=[]
-    for line in lines:
-        list_of_current.append(int(line))
-    list_of_top.append(list_of_current)
+list_of_trees=[]
+list_of_roots=[]
+list_of_cat=[]
+#path=raw_input("Enter category file path ")
+path=create_filename_current_directory("Datasets/cat_hier_DMOZ.txt")
+#path=create_filename_current_directory("Datasets/trial_hier.txt")
+f = open(path, 'r')
+ctr=0
+lines=f.readlines()
+for line in lines:
     
-filepath=create_filename_current_directory("Datasets/trainDmoz.txt")
-X_train, y_train = load_svmlight_file(filepath)
+    words=line.split()
+    nodes=[]
+    tree_index=-1
+    tree_index2=-1 
+
+    for word in words:
+        nodes.append(int(word))
+    
+    
+    #Create two checks to see if the edge connects to different subtrees  
+    index=0
+    for tree in list_of_trees:
+        if nodes[0] in tree:
+            tree_index=index
+        if nodes[1] in tree:
+            tree_index2=index
+        index=index+1 
+         
+    if ((tree_index2>-1) and (tree_index>-1)):
+        #Two sperate trees have now been connected by th current egde
+        #Merge the trees elements,and their file data
+        #set the root to the higher node 
+        print len(list_of_trees)
+        print len(list_of_cat)
+        elements_union = list(set().union(list_of_trees[tree_index],list_of_trees[tree_index2]))    
+        cat_union = list(set().union(list_of_cat[tree_index],list_of_cat[tree_index2]))    
+        
+        
+        tree1=list_of_trees[tree_index]
+        tree2=list_of_trees[tree_index2]
+        list_of_trees.remove(tree1)
+        list_of_trees.remove(tree2)
+        list_of_trees.append(elements_union)
+        cat1=list_of_cat[tree_index]
+        cat2=list_of_cat[tree_index2]
+        list_of_cat.remove(cat1)
+        list_of_cat.remove(cat2)
+        list_of_cat.append(cat_union)
+        
+        
+        print "Old roots: "
+        print list_of_roots        
+        new_root=list_of_roots[tree_index]
+        list_of_roots.remove(list_of_roots[tree_index2])
+        list_of_roots.remove(new_root)
+        list_of_roots.append(new_root)
+        print "New roots: "
+        print list_of_roots
+        
+        print "New elements of tree: "
+        print elements_union
+    else:
+        #Set current node from 0,1
+        if (tree_index==-1) and (tree_index2>-1):
+             tree_index=tree_index2
+        
+        #if edge not connected to any existing tree     
+        if tree_index==-1:
+             cat=[]
+             cat.append(line)
+             tree=[]
+             for node in nodes:
+                 tree.append(node)
+             list_of_cat.append(cat)
+             list_of_roots.append(nodes[0])
+             list_of_trees.append(tree)
+             print "Set parent "+str(nodes[0])
+        else: 
+             #add to existing tree
+             current_tree=list_of_trees[tree_index]
+             list_of_cat[tree_index].append(line)
+             if nodes[0] not in current_tree:
+                 old=list_of_roots[tree_index]
+                 list_of_roots[tree_index]=nodes[0]
+                 print "Changed "+str(old)+" to "+str(list_of_roots[tree_index])                
+             print "Old tree: "
+             print current_tree
+             for node in nodes:
+                 if node not in current_tree:
+                     current_tree.append(node)
+             print "New tree: "
+             print current_tree         
+
+#path=raw_input("Enter dataset path ")             
 
 index=0
-for y in y_train:
-    top_index=0
-    for top in list_of_top:
-        if y in top:
-           list_of_data[top_index].append(X_train[index])
-           list_of_data[top_index].append(y)
-           
+for cat in list_of_cat:
+    filepath=create_filename_current_directory("Datasets/hierarchy_"+str(index))
+    
+    write_str_list_to_file(filepath,cat)
+    index=index+1  
+
+index=0
+for tree in list_of_trees:
+    filepath=create_filename_current_directory("Datasets/all_categories_"+str(index))
+    write_list_to_file(filepath,tree)
+    index=index+1                  
+    
+filepath=create_filename_current_directory("Datasets/Root_Nodes")
+write_list_to_file(filepath,list_of_roots)
+
